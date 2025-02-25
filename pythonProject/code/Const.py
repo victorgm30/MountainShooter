74a1b77034f68f22896753bf56cbecfd4a983391
VictorGM <victorg.moreira92@gmail.com>
2025-02-24 22:34:48 -0300

    Creating menu interactions Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- pythonProject/code/Const.py
+++ pythonProject/code/Const.py
@@ -2,6 +2,18 @@
 COLOR_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_BLACK = (0, 0, 0)
+C_YELLOW = (255, 255, 0)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
