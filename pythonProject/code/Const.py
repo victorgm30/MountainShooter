3c75357a307a97c28b3f22d13d1b983d04967d20
VictorGM <victorg.moreira92@gmail.com>
2025-02-28 13:12:39 -0300

    Creating and Moving Players and Enemies

--- pythonProject/code/Const.py
+++ pythonProject/code/Const.py
@@ -1,10 +1,13 @@
 # C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_BLACK = (0, 0, 0)
 C_YELLOW = (255, 255, 0)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -13,6 +16,10 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
 # M
@@ -21,6 +28,21 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
+# S
+SPAWN_TIME = 4000
 
 # W
 WIN_WIDTH = 576
--- pythonProject/code/entity.py
+++ pythonProject/code/entity.py
@@ -11,6 +11,7 @@ class Entity(ABC):
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
 
+
     @abstractmethod
     def move(self, ):
         pass
